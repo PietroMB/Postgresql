c1fbf98dea57b907450d5c649e8cafef1fb97f39
PietroMB <orsopk@gmail.com>
2017-02-21 20:09:38 +0100

    aggiunte query

--- Biblioteca/biblioteca.sql
+++ Biblioteca/biblioteca.sql
@@ -1,7 +1,9 @@
 drop schema if exists biblioteca cascade;
+
 create schema biblioteca;
 
 set search_path to biblioteca;
+
 create table scrittore(
 	nome varchar(50) primary key,
 	sesso char check(sesso='M' or sesso='F'),
@@ -46,4 +48,21 @@ select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala='A
 
 select l.titolo, g.sala from libro as l, generi as g where l.genere=g.nome;
 
+select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A';
 
+select s.nome from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A' group by (s.nome);
+
+select s.nome from scrittore as s, ha_letto as h, libro as l, socio as so where s.nome=l.autore and l.ISBN=h.ISBN and so.id_socio=h.socio and so.sesso='F';
+
+select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
+
+select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
+
+select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala=(select ge.sala from generi as ge, libro as li where li.genere=ge.nome and li.titolo='Ossi di seppia');
+
+select autore from libro where titolo<(select titolo from libro where ISBN='88-55-55555-5') order by titolo;
+
+select l.autore from libro as l, ha_letto as h where h.ISBN=l.ISBN and l.titolo<(select titolo from libro where ISBN='88-55-55555-5') order by (l.titolo);
+
+
+/*d,f da fare*/
