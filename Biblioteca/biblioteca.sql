24fff1c08635ddfe6deb22ec224f52fa27141968
PietroMB <orsopk@gmail.com>
2017-02-21 20:18:59 +0100

    aggiunte query

--- Biblioteca/biblioteca.sql
+++ Biblioteca/biblioteca.sql
@@ -42,27 +42,30 @@ create table ha_letto(
 \copy socio from Socio.txt;
 \copy ha_letto (socio, ISBN) from Ha_letto.txt;
 
-select s.nome from socio as s, ha_letto as h where h.socio=s.id_socio and s.sesso='F' group by(s.nome); 
+/*a*/select s.nome from socio as s, ha_letto as h where h.socio=s.id_socio and s.sesso='F' group by(s.nome); 
 
-select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala='A';
+/*b*/select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala='A';
 
-select l.titolo, g.sala from libro as l, generi as g where l.genere=g.nome;
+/*c*/select l.titolo, g.sala from libro as l, generi as g where l.genere=g.nome;
 
-select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A';
+/*e*/select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A';
 
-select s.nome from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A' group by (s.nome);
+/*g*/select s.nome from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala='A' group by (s.nome);
 
-select s.nome from scrittore as s, ha_letto as h, libro as l, socio as so where s.nome=l.autore and l.ISBN=h.ISBN and so.id_socio=h.socio and so.sesso='F';
+/*h*/select s.nome from scrittore as s, ha_letto as h, libro as l, socio as so where s.nome=l.autore and l.ISBN=h.ISBN and so.id_socio=h.socio and so.sesso='F';
 
-select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
+/*i*/select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.ISBN=h.ISBN and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
 
-select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
+/*j*/select s.id_socio from socio as s, generi as g, libro as l, ha_letto as h where s.id_socio=h.socio and l.genere=g.nome and g.sala != 'B' group by(s.id_socio);
 
-select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala=(select ge.sala from generi as ge, libro as li where li.genere=ge.nome and li.titolo='Ossi di seppia');
+/*k*/select l.titolo from libro as l, generi as g where l.genere=g.nome and g.sala=(select ge.sala from generi as ge, libro as li where li.genere=ge.nome and li.titolo='Ossi di seppia');
 
-select autore from libro where titolo<(select titolo from libro where ISBN='88-55-55555-5') order by titolo;
+/*l*/select autore from libro where titolo<(select titolo from libro where ISBN='88-55-55555-5') order by titolo;
 
-select l.autore from libro as l, ha_letto as h where h.ISBN=l.ISBN and l.titolo<(select titolo from libro where ISBN='88-55-55555-5') order by (l.titolo);
+/*m*/select l.autore from libro as l, ha_letto as h where h.ISBN=l.ISBN and l.titolo<(select titolo from libro where ISBN='88-55-55555-5') order by (l.titolo);
+
+/*n*/select s.id_socio from socio as s, ha_letto as h where h.socio=s.Id_socio and h.ISBN<(select MIN(li.ISBN) from libro as li, generi as ge where ge.nome=li.genere and ge.sala='B') group by s.id_socio;
+
 
 
 /*d,f da fare*/
